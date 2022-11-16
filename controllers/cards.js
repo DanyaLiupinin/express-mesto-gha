7ebb3d6eeabb6bc71164c161a5dcd2262fc8d269
Danya Kovan <mr.kovan_2002@mail.ru>
2022-11-16 19:10:25 +0100

    errors fixed

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ const getCards = (req, res) => {
     .then((data) => {
       res.status(200).send(data);
     })
-    .catch((err) => {
+    .catch(() => {
       res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
@@ -29,15 +29,15 @@ const createCard = (req, res) => {
 
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .orFail(() => new Error('Карточка не найдена'))
+    .orFail(() => new Error({ message: 'Карточка не найдена' }))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send('Карточка не найдена');
+        res.status(400).send({ message: 'Карточка не найдена' });
       } else if (err.statusCode === 404) {
-        res.status(404).send('Карточка не найдена');
+        res.status(404).send({ message: 'Карточка не найдена' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
@@ -47,15 +47,15 @@ const deleteCard = (req, res) => {
 const putLike = (req, res) => {
   const userId = req.user._id;
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: userId } }, { new: true })
-    .orFail(() => new Error('Карточка не найдена'))
+    .orFail(() => new Error({ message: 'Карточка не найдена' }))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send('Карточка не найдена');
+        res.status(400).send({ message: 'Карточка не найдена' });
       } else if (err.statusCode === 404) {
-        res.status(404).send('Карточка не найдена');
+        res.status(404).send({ message: 'Карточка не найдена' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
@@ -65,15 +65,15 @@ const putLike = (req, res) => {
 const deleteLike = (req, res) => {
   const userId = req.user._id;
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: userId } }, { new: true })
-    .orFail(() => new Error('Карточка не найдена'))
+    .orFail(() => new Error({ message: 'Карточка не найдена' }))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send('Карточка не найдена');
+        res.status(400).send({ message: 'Карточка не найдена' });
       } else if (err.statusCode === 404) {
-        res.status(404).send('Карточка не найдена');
+        res.status(404).send({ message: 'Карточка не найдена' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
--- controllers/users.js
+++ controllers/users.js
@@ -5,22 +5,22 @@ const getUsers = (req, res) => {
     .then((data) => {
       res.status(200).send(data);
     })
-    .catch((err) => {
+    .catch(() => {
       res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
 
 const getUser = (req, res) => {
   User.findById(req.params.userId)
-    .orFail(() => new Error('Пользователь не найден'))
+    .orFail(() => new Error({ message: 'Пользователь не найден' }))
     .then((user) => {
       res.send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send('Пользователь не найден');
+        res.status(400).send({ message: 'Пользователь не найден' });
       } else if (err.statusCode === 404) {
-        res.status(404).send('Пользователь не найден');
+        res.status(404).send({ message: 'Пользователь не найден' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
@@ -47,15 +47,15 @@ const updateUser = (req, res) => {
   const userId = req.user._id;
 
   User.findByIdAndUpdate({ _id: userId }, { name, about })
-    .orFail(() => new Error('Пользователь не найден'))
+    .orFail(() => new Error({ message: 'Пользователь не найден' }))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send('Пользователь не найден');
+        res.status(400).send({ message: 'Пользователь не найден' });
       } else if (err.statusCode === 404) {
-        res.status(404).send('Пользователь не найден');
+        res.status(404).send({ message: 'Пользователь не найден' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
@@ -66,15 +66,15 @@ const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
   User.findByIdAndUpdate({ _id: userId }, { avatar })
-    .orFail(() => new Error('Пользователь не найден'))
+    .orFail(() => new Error({ message: 'Пользователь не найден' }))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send('Пользователь не найден');
+        res.status(400).send({ message: 'Пользователь не найден' });
       } else if (err.statusCode === 404) {
-        res.status(404).send('Пользователь не найден');
+        res.status(404).send({ message: 'Пользователь не найден' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка' });
       }
