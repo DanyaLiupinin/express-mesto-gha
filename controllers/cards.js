4652efdf73bd8946c2b178bcfd2a72ed748b4d20
Danya Kovan <mr.kovan_2002@mail.ru>
2022-11-16 16:50:10 +0100

    +card controller error validation

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,7 +6,7 @@ const getCards = (req, res) => {
       res.status(200).send(data);
     })
     .catch((err) => {
-      res.status(500).send(err);
+      res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
 
@@ -19,39 +19,64 @@ const createCard = (req, res) => {
       res.status(201).send(data);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Введены некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
 
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
+    .orFail(() => new Error('Карточка не найдена'))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.name === 'CastError') {
+        res.status(400).send('Неверный id карточки');
+      } else if (err.statusCode === 404) {
+        res.status(404).send('Карточка не найдена');
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
 
 const putLike = (req, res) => {
   const userId = req.user._id;
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: userId } }, { new: true })
+    .orFail(() => new Error('Карточка не найдена'))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.name === 'CastError') {
+        res.status(400).send('Неверный id карточки');
+      } else if (err.statusCode === 404) {
+        res.status(404).send('Карточка не найдена');
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
 
 const deleteLike = (req, res) => {
   const userId = req.user._id;
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: userId } }, { new: true })
+    .orFail(() => new Error('Карточка не найдена'))
     .then((data) => {
       res.status(201).send(data);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.name === 'CastError') {
+        res.status(400).send('Неверный id карточки');
+      } else if (err.statusCode === 404) {
+        res.status(404).send('Карточка не найдена');
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
 
