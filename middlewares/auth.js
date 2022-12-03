ddba1864ecad6c28e4f80d2f35ef3b646725029d
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-03 12:15:03 +0100

    middlewares review fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,17 @@
 const jwt = require('jsonwebtoken');
+const UnauthorizatedError = require('../errors/UnauthorizedError');
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers; // извлекаем заголовок с авторизацией из запрсоа
   if (!authorization || !authorization.startsWith('Bearer ')) { // проверяем есть ли он и с чего начинается
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new UnauthorizatedError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', ''); // выкидываем из заголовка bearer
   let payload;
   try {
     payload = jwt.verify(token, 'very-secret-key'); // верифицирует токен и возвращает пэйлоуд
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new UnauthorizatedError('Необходима авторизация'));
   }
   req.user = payload; // записываем пейлоуд в объект запроса
 
